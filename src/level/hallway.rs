219abca5ef5711d2a8f18fa99eaaf42c9d50116b
generic-coder-1 <abdulgzia@gmail.com>
2024-01-14 22:53:47 -0500

    created control rects and hallways

---
+++ src/level/hallway.rs
@@ -0,0 +1,34 @@
+use crate::renderer::texture::TextureId;
+
+use super::control_rect::ControlRectId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct HallWay {
+    pub start: ControlRectId,
+    pub others: Vec<HallWaySegment>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct HallWaySegment {
+    pub rect_id: ControlRectId,
+    pub sides: TextureId,
+    pub roof: TextureId,
+    pub floor: TextureId,
+    pub flat_project: bool,
+}
+
+impl HallWay {
+    pub fn new(start: ControlRectId, end: ControlRectId, texture_id: TextureId) -> Self {
+        Self {
+            start,
+            others: vec![HallWaySegment {
+                rect_id: end,
+                flat_project: false,
+                sides: texture_id.clone(),
+                roof: texture_id.clone(),
+                floor: texture_id.clone(),
+            }],
+        }
+    }
+}
--- src/level/level.rs
+++ src/level/level.rs
@@ -5,21 +5,26 @@ use crate::{camer_control::CameraController, renderer::camera::Camera};
 use cgmath::{Basis3, Point3, Rad, Rotation3};
 use serde::{Deserialize, Serialize};
 
-use super::mesh::{Meshable, Mesh};
+use super::{mesh::{Meshable, Mesh}, control_rect::{ControlRectId, ControlRect}, hallway::HallWay};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct LevelData {
     pub start_camera: CameraController,
+    pub control_rects:HashMap<ControlRectId,ControlRect>,
+    pub hallways:Vec<HallWay>,
 }
 
 impl LevelData {
     pub fn new() -> Self {
+        let control_rects = HashMap::new();
         Self {
             start_camera: CameraController::new(
                 4.0,
                 0.4,
                 Camera::new(Point3::new(0.0, 2.0, 0.0), Rad(0.0), Rad(0.0)),
             ),
+            control_rects,
+            hallways:vec![],
         }
     }
 }
